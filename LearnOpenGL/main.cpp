23246397e6fb26fb0de81d52d2f2b9002d80e557
Magnus Halvorsen <MagnusHalvorsen1991@gmail.com>
2017-12-25 16:56:38 +0100

    getting-started/hello-triangle

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,6 +1,42 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <assert.h>
+
+char vertexShaderSource[] = "#version 330 core\n\
+layout(location = 0) in vec3 aPos;\n\
+\n\
+void main() {\n\
+	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n\
+}";
+
+char fragmentShaderSource[] = "#version 330 core\n\
+out vec4 FragColor;\n\
+\n\
+void main() {\n\
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n\
+}";
+
+float triangle[] = {
+	-0.5f, -0.5f, 0.0f, // left  
+	0.5f, -0.5f, 0.0f, // right 
+	0.0f,  0.5f, 0.0f  // top   
+};
+
+int triangleIndices[] = {
+	0, 1, 2
+};
+
+float rectangle[] = {
+	0.5f,  0.5f, 0.0f,  // top right
+	0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+};
+unsigned int rectangleIndices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -11,34 +47,119 @@ void processInput(GLFWwindow *window) {
 		glfwSetWindowShouldClose(window, true);
 }
 
-int main() {
+GLuint createVBO(float *vertices, unsigned int length) {
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, length, vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	return VBO;
+}
+
+GLuint createEBO(unsigned int *indices, int indicesLength) {
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesLength, indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	return EBO;
+}
+
+GLuint createShader(char *sourceCode, GLuint shaderType) {
+	GLuint shader;
+	shader = glCreateShader(shaderType);
+	glShaderSource(shader, 1, &sourceCode, NULL);
+	glCompileShader(shader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		assert(false);
+	}
+
+	return shader;
+}
+
+GLFWwindow* createWindow(unsigned int xDim, unsigned int yDim) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(xDim, yDim, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return -1;
+		assert(false);
 	}
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
+		assert(false);
 	}
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, xDim, yDim);
+	return window;
+}
 
-	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(window);
+GLuint linkShaderProgram(GLuint vertexShader, GLuint fragmentShader) {
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		assert(false);
+	}
+
+	return shaderProgram;
+}
+
+int main() {
+
+	auto window = createWindow(800, 600);
+
+	auto vertexShader = createShader(vertexShaderSource, GL_VERTEX_SHADER);
+	auto fragmentShader = createShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+	auto shaderProgram = linkShaderProgram(vertexShader, fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	VBO = createVBO(rectangle, sizeof(rectangle));
+	EBO = createEBO(rectangleIndices, sizeof(rectangleIndices));
+	glBindVertexArray(0);
+
 
 	while (!glfwWindowShouldClose(window)) {
+
 		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw our first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
